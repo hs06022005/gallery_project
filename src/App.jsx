054092ad199f2b933cc2a403a2703aa0bf51b2db
Himanshu <harsh06022005@gmail.com>
2026-04-20 14:53:39 +0530

    Error-page Added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Routes, Route } from 'react-router-dom'
 import Footer from './Component/Footer'
 import Home from './Pages/Home' 
 import Contact from './Pages/Contact'
+import Error from './Pages/Error'
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/Image" element={<Image />} />
           <Route path="/Contact" element={<Contact />} />
+          <Route path="*" element={<Error />} />
         </Routes>
         <Footer />
     </div>
---
+++ src/Pages/Error.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Error = () => {
+    return (
+        <div className='bg-[url(https://images.unsplash.com/photo-1611890798517-07b0fcb4a811?crop=entropy&cs=srgb&fm=jpg&ixid=M3wzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NzY2NzY0ODd8&ixlib=rb-4.1.0&q=85)] bg-cover bg-center h-screen text-white flex items-center justify-center text-4xl'>
+        </div>
+    )
+}
+
+export default Error
